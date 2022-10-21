83ea9b6848060eadd539430b3a68df37a4249464
Tomas Mendez <tmendez@es.consulting>
2022-10-21 15:24:23 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+console.log('Script started successfully');
+
+let currentPopup: any = undefined;
+
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+    
+    WA.room.onEnterLayer('clockZone').subscribe(() => {
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
+    })
+
+    WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+    
+}).catch(e => console.error(e));
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
+let helloWorldPopup: any=undefined;
+
+// Open the popup when we enter a given zone
+WA.room.onEnterLayer("popups").subscribe(() => {
+    helloWorldPopup = WA.ui.openPopup("popup1", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("popups").subscribe(() => {
+    helloWorldPopup.close();
+})
